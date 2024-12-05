4ba0c5843e308aed1da53e3d122df50a4946b3c9
Jonan Scheffler <jonanscheffler@gmail.com>
2024-12-04 19:50:09 -0800

    Update Session so the build works?

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,18 +1,13 @@
-import { SessionProvider } from "next-auth/react"
-import "./styles.css"
+import type { AppProps } from "next/app";
+import type { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 
-import type { AppProps } from "next/app"
-import type { Session } from "next-auth"
-
-// Use of the <SessionProvider> is mandatory to allow components that call
-// `useSession()` anywhere in your application to access the `session` object.
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps<{ session: Session }>) {
+function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
-  )
+  );
 }
+
+export default MyApp;
